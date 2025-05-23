ce9dbac7ab3ab28e4ac6b7c4dec1f28d768c65b1
Armando-Chavez91 <124794857+Armando-Chavez91@users.noreply.github.com>
2025-05-22 18:30:27 -0600

    Update test_tarea.py

--- test_tarea.py
+++ test_tarea.py
@@ -1,1 +1,16 @@
+import tarea
+import pandas as pd
+
+df = pd.DataFrame({
+    "Survived": [1, 0, 1, 1],
+    "Sex": ["male", "female", "female", "male"],
+    "Age": [22, 38, 26, 35],
+    "Pclass": [3, 1, 3, 2]
+})
+
+def test_probabilidad_basica():
+    assert tarea.calcular_probabilidades(df) == 0.75
+
+def test_probabilidad_condicional():
+    assert tarea.probabilidad_condicional(df, "Sex", "female") == 0.5
 
